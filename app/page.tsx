8eef8e6d7592e4b58150ef91e21c8c357c6a317f
Rohit <rohitnegi1108@gmail.com>
2025-06-13 16:15:04 +0530

    feat: add pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/home/hero-section";
 import BgGradient from "@/components/common/Bg-gradient"; 
 import DemoSection from "@/components/home/demo-section";
 import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
 export default function Home() {
   return (
     <div className="relative w-full ">
@@ -11,11 +12,12 @@ export default function Home() {
         <HeroSection />
         <DemoSection/>
         <HowItWorksSection/>
+        <PricingSection/>
       </div>
       
       
       
-      {/*  <PricingSection/> */}
+      
       {/*  <CTASection/> */}
     </div>
   );

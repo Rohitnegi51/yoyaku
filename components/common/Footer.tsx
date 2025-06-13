f958dcd2d6989a0c16844d0bfd1797390501d63a
Rohit <rohitnegi1108@gmail.com>
2025-06-13 21:39:30 +0530

    add successful

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 py-12">Footer</footer>
+    <footer className="bg-gray-50 py-12">Feeter</footer>
   )
 }
 

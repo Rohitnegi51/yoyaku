08a1ba82e8b01f1e60846c25858df66bd0889913
Rohit <rohitnegi1108@gmail.com>
2025-06-14 17:08:56 +0530

    feat : add UploadThing integration with Zod file validation and clerk auth

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,15 +1,68 @@
 "use client";
 import UploadFormInput from "@/components/upload/upload-form-input";
-
-
-
-export default function UploadForm(){
-    const handleSubmit =()=>{
-        console.log("submitted");
-    }
-    return(
-        <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
-            <UploadFormInput onSubmit = {handleSubmit}/>
-        </div>
+import React from "react";
+import { z } from "zod";
+import { useUploadThing } from "@/utils/uploadthing";
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid File" })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File size must be less than 20MB"
     )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF !"
+    ),
+});
+
+export default function UploadForm() {
+
+    const {startUpload , routeConfig} = useUploadThing("pdfUploader",{
+        onClientUploadComplete:()=>{
+            console.log("upload successfully!");
+        },
+        onUploadError : (err)=>{
+            console.error("error occurred while uploading",err);
+        },
+        onUploadBegin :({file})=>{
+            console.log("upload has begun for ", file);
+        }
+    })
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+
+    //validate the fields
+    //schema validation using zod
+    const validatedFields = schema.safeParse({file});
+
+    console.log(validatedFields)
+
+    if(!validatedFields.success){
+        console.log(
+            validatedFields.error.flatten().fieldErrors.file?.[0] ?? 'Invalid file'
+        )
+        return;
+    }
+
+    //upload the file to uploadthing
+    const resp = await startUpload([file]);
+    if(!resp){
+        return;
+    }
+
+    //parse the pdf using lang chain
+    //summarize the pdf using AI
+    //save the summary to the database
+    //redirect to the id summary page
+  };
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
 }
---
+++ utils/uploadthing.ts
@@ -0,0 +1,7 @@
+
+
+import type { OurFileRouter } from "@/app/api/uploadthing/core"
+import { generateReactHelpers } from "@uploadthing/react";
+
+export const {useUploadThing} = generateReactHelpers<OurFileRouter>();
+

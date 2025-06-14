38a1f91738349fe7bf78b51d5ba0d02d2762ed9d
Rohit <rohitnegi1108@gmail.com>
2025-06-14 12:25:47 +0530

    feat: pdf upload section

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,14 @@
+import BgGradient from '@/components/common/Bg-gradient'
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+  <section className='flex justify-center item-center lg:min-h-[40vh]'>
+    <div className="py-12 lg:py-24 max-w-5xl mx-auto sm:px-6 lg:px-8 lg:pt-12">
+        <BgGradient className='from-rose-400 via-rose-300 to-orange-200'/>
+        <SignUp />
+    </div>
+    
+  </section>)
+  
 }
